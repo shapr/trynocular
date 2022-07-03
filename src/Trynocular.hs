03d6a575121f247029543057a768f30dd1b11c17
Chris Smith <cdsmith@gmail.com>
2022-07-03 15:53:47 -0400

    Export PartialKeySet from Trynocular

--- src/Trynocular.hs
+++ src/Trynocular.hs
@@ -3,9 +3,11 @@ module Trynocular
   ( module Trynocular.Generator,
     module Trynocular.Key,
     module Trynocular.Generable,
+    module Trynocular.PartialKeySet
   )
 where
 
 import Trynocular.Generable
 import Trynocular.Generator
 import Trynocular.Key
+import Trynocular.PartialKeySet
