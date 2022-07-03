c5f7ba39ff06802f04432675f2eaac41f94aa0f2
Shae Erisson <shae@scannedinavian.com>
2022-07-03 17:55:15 -0400

    add observedCoverage via HPC

--- src/Trynocular/TestHarness.hs
+++ src/Trynocular/TestHarness.hs
@@ -4,6 +4,8 @@ import Control.Monad (when)
 import Trynocular.Generator
 import Trynocular.Key
 import Trynocular.PartialKeySet (PartialKeySet)
+import Trace.Hpc.Reflect (examineTix)
+import Trace.Hpc.Tix
 import Trynocular.PartialKeySet qualified as PartialKeySet
 
 data CoverageExpectation = CoverageExpectation
@@ -43,8 +45,11 @@ updateCoverageExpectation observed (CoverageExpectation mean var) =
     var' = alpha * observedVar + (1 - alpha) * var
     alpha = 0.1
 
-updateGenerator :: Double -> PartialKey -> Generator a -> Generator a
-updateGenerator _ _ gen = gen -- TODO
+updateGenerator :: Double -- ^ stddevs above or below
+                -> PartialKey -- ^ demanded portion of the key
+                -> Generator a -- ^ generator to update
+                -> Generator a -- ^ new updated generator
+updateGenerator score pkey gen = gen
 
 testHarness :: TestState -> IO ()
 testHarness state@(TestState n generator action usedKeys coverage) = do
@@ -52,15 +57,38 @@ testHarness state@(TestState n generator action usedKeys coverage) = do
   if key `PartialKeySet.member` usedKeys
     then testHarness state
     else do
-      (pkey, ()) <- spy key (action . fromKey generator)
-      let observedCoverage = 0.1 -- TODO
+      (pkey, ((), observedCoverage)) <- spy key (tixCountWrapper . action . fromKey generator)
       let score = (observedCoverage - coverageMean coverage)
             / sqrt (coverageVariance coverage)
       let generator' = updateGenerator score pkey generator
-      let coverage' = updateCoverageExpectation observedCoverage coverage
+      let coverage' = updateCoverageExpectation (observedCoverage) coverage
       let usedKeys' = PartialKeySet.insert pkey usedKeys
       let state' = TestState (n + 1) generator' action usedKeys' coverage'
       when (shouldContinue state') $ testHarness state'
 
 shouldContinue :: TestState -> Bool
 shouldContinue (TestState n _ _ _ _) = n < 1000
+
+tixCountWrapper :: IO a -> IO (a, Double)
+tixCountWrapper a = do
+  preTix <- examineTix
+  a' <- a
+  afterTix <- examineTix
+  pure (a', realToFrac (additionalTix preTix afterTix) / realToFrac (tixModuleSize preTix))
+
+additionalTix :: Tix -> Tix -> Int
+additionalTix old new = tixModuleCount new - tixModuleCount old
+
+-- How many regions were executed at least once for this module?
+tixCount :: TixModule -> Int
+tixCount (TixModule _ _ _ regions) = sum $ 1 <$ filter (> 0) regions
+
+-- How many regions were executed at least once for all these modules?
+tixModuleCount :: Tix -> Int
+tixModuleCount (Tix ms) = sum $ map tixCount ms
+
+tixModuleSize :: Tix -> Int
+tixModuleSize (Tix ms) = sum $ tixSize <$> ms
+
+tixSize :: TixModule -> Int
+tixSize (TixModule _ _ s _) = s
